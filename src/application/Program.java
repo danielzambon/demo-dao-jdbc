465c7a60ce3711179546056194dff94dfbaa7748
danielzambon <danielfzambon@gmail.com>
2021-08-23 22:52:39 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,17 +26,23 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n === Test 3: Seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		System.out.println("\n === Test 3: Seller Insert ===");
+
+		System.out.println("\n === Test 4: Seller Insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new id = " + newSeller.getId());
+
+		System.out.println("\n === Test 5: Seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed: ");
 
 	}
 
